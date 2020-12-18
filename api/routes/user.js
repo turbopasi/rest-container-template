1ab05092d69d5dc2eedad3f308263d1dee2c0116
turbopasi <office@pascal-lamers.at>
2020-12-18 22:00:55 +0100

    Added simple controllers for user route

--- api/routes/user.js
+++ api/routes/user.js
@@ -12,15 +12,25 @@ const schema = Joi.object({
 module.exports = (parentRouter) => {
 
   router.post('/', async (req, res) => {
-
     const body = await schema.validateAsync(req.body, {stripUnknown : true});
     const newUser = await userServiceInstance.Create(body);
     return res.status(201).json(newUser);
-
   });
 
-  router.get('/', async(req, res) => {
-    return res.status(200).json("GET /user")
+  router.get('/:id', async(req, res) => {
+    const user = await userServiceInstance.FindById(req.params.id);
+    return res.status(200).json(user);
+  });
+
+  router.put('/:id', async(req, res) => {
+    const body = await schema.validateAsync(req.body, {stripUnknown : true});
+    const user = await userServiceInstance.FindByIdAndUpdate(req.params.id, body);
+    return res.status(200).json(user);
+  });
+
+  router.delete('/:id', async(req, res) => {
+    const user = await userServiceInstance.FindByIdAndDelete(req.params.id);
+    return res.status(200).json(user);
   });
 
   parentRouter.use('/user', router);
